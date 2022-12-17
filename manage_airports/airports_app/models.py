7e62101e95b59c70ddfae1f28539fd194825d376
andreykhlivniuk <khlivnia@tcd.ie>
2022-12-17 16:37:38 +0200

    add Airport model

--- manage_airports/airports_app/models.py
+++ manage_airports/airports_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    ident=models.CharField(max_length=100)
+    type=models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
+    latitude_deg=models.FloatField(blank=True, default=0)
+    longitude_deg=models.FloatField(blank=True, default=0)
+    elevation_ft=models.IntegerField(blank=True, default=None, null=True)
+    continent=models.CharField(max_length=100, blank=True, default="")
+    iso_country=models.CharField(max_length=100, blank=True, default="")
+    iso_region=models.CharField(max_length=100, blank=True, default="")
+    municipality=models.CharField(max_length=100, blank=True, default="")
+    scheduled_service=models.CharField(max_length=100, blank=True, default="")
+    gps_code=models.CharField(max_length=100, blank=True, default="")
+    iata_code=models.CharField(max_length=100, blank=True, default="")
+    local_code=models.CharField(max_length=100, blank=True, default="")
+    home_link=models.URLField(blank=True, default="")
+    wikipedia_link=models.URLField(blank=True, default="")
+    keywords=models.CharField(max_length=100, blank=True, default="")
+
+    def __str__(self):
+        return self.name
