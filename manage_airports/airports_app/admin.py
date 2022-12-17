40136b803a0b02a942fa63e656b2d75ee905b909
andreykhlivniuk <khlivnia@tcd.ie>
2022-12-17 16:50:19 +0200

    setted admin, made migrations

--- manage_airports/airports_app/admin.py
+++ manage_airports/airports_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Airport
 # Register your models here.
+
+
+
+class AirportAdmin(admin.ModelAdmin):
+    search_fields = ("name","ident")
+
+admin.site.register(Airport, AirportAdmin)
