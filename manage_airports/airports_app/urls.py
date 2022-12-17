5d98aa9d8bae35b4103822fd119f39a58ce8d7a8
andreykhlivniuk <khlivnia@tcd.ie>
2022-12-17 17:01:39 +0200

    added rest api get all and get by ident

--- manage_airports/airports_app/urls.py
+++ manage_airports/airports_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('airports/', views.airport_list),
+    path('airports/<str:ident>/', views.airport_ident)
 ]
--- manage_airports/airports_app/views.py
+++ manage_airports/airports_app/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Airport
+from .serializers import AirportSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 def index(request):
     
     return render(request, 'index.html')
+
+@api_view(['GET'])
+def airport_list(request):
+    airports = Airport.objects.all()
+    serializer = AirportSerializer(airports, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def airport_ident(request, ident):
+    try:
+        airport = Airport.objects.get(ident=ident)
+    except Airport.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = AirportSerializer(airport)
+    return Response(serializer.data)
