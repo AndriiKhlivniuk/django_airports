904da120c536bab7eff4bd44541b62b352d08fc8
andreykhlivniuk <khlivnia@tcd.ie>
2023-01-17 15:51:04 +0200

    changed code style

--- manage_airports/airports_app/tasks.py
+++ manage_airports/airports_app/tasks.py
@@ -15,12 +15,12 @@ def add_csv_data():
 
         updated_values = dict(
             ident = row.ident, type = row.type, name = row['name'], 
-            latitude_deg= row.latitude_deg, longitude_deg = row.longitude_deg, 
+            latitude_deg = row.latitude_deg, longitude_deg = row.longitude_deg, 
             elevation_ft = row.elevation_ft, continent = row.continent,
             iso_country = row.iso_country, iso_region = row.iso_region,
             municipality = row.municipality, scheduled_service = row.scheduled_service,
-            gps_code=row.gps_code, iata_code = row.iata_code, local_code = row.local_code,
-            home_link = row.home_link, wikipedia_link=row.wikipedia_link,
+            gps_code = row.gps_code, iata_code = row.iata_code, local_code = row.local_code,
+            home_link = row.home_link, wikipedia_link = row.wikipedia_link,
             keywords = row.keywords)
 
         Airport.objects.update_or_create(id = row.id, defaults = updated_values)
