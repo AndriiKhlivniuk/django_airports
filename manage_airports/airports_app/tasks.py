e8dcb132cd632b5f8584001daac22bba3608320b
andreykhlivniuk <khlivnia@tcd.ie>
2023-01-17 15:46:27 +0200

    task code style modified

--- manage_airports/airports_app/tasks.py
+++ manage_airports/airports_app/tasks.py
@@ -7,17 +7,22 @@ def add_csv_data():
     url = 'https://davidmegginson.github.io/ourairports-data/airports.csv'
     df = pd.read_csv(url)
     print(df.keys())
-    for i,row in df.iterrows():
+
+    for i, row in df.iterrows():
 
         if not row.elevation_ft.is_integer():
             row.elevation_ft = None
 
-        updated_values=dict(ident = row.ident, type = row.type, name=row['name'], latitude_deg= row.latitude_deg, 
-        longitude_deg = row.longitude_deg, elevation_ft = row.elevation_ft, continent = row.continent, iso_country = row.iso_country,
-        iso_region = row.iso_region, municipality = row.municipality, scheduled_service = row.scheduled_service, gps_code=row.gps_code,
-        iata_code = row.iata_code, local_code = row.local_code, home_link = row.home_link, wikipedia_link=row.wikipedia_link,
-        keywords = row.keywords)
+        updated_values = dict(
+            ident = row.ident, type = row.type, name=row['name'], 
+            latitude_deg= row.latitude_deg, longitude_deg = row.longitude_deg, 
+            elevation_ft = row.elevation_ft, continent = row.continent,
+            iso_country = row.iso_country, iso_region = row.iso_region,
+            municipality = row.municipality, scheduled_service = row.scheduled_service,
+            gps_code=row.gps_code, iata_code = row.iata_code, local_code = row.local_code,
+            home_link = row.home_link, wikipedia_link=row.wikipedia_link,
+            keywords = row.keywords)
 
-        Airport.objects.update_or_create(id = row.id, defaults=updated_values)
+        Airport.objects.update_or_create(id = row.id, defaults = updated_values)
         
 
